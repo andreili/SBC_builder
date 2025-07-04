fe18f2da2a9fd22112695f6aff93ba7222560359
andreili <andreil499@gmail.com>
2025-07-05 00:37:35 +0200

    Cleanup.

--- tmp/back.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-now=$(date +"%Y_%m_%d")
-BASEDIR=$(dirname "$0")
-XZ_OPT="-9 --extreme --threads=0" tar -cvJpf ../back_os_${now}.tar.xz --exclude-from=${BASEDIR}/excl_root.lst .
-#XZ_OPT="-9 --extreme --threads=0" tar -cvJpf ../back_home_${now}.tar.xz ./home
-#XZ_OPT="-9 --extreme --threads=0" tar -cvJpf ../back_${now}.tar.xz .
