4558a136ed323a5f5d176fd0a0be9e84237fefd9
Andrey Leonchikov <andreil499@gmail.com>
2025-12-07 16:34:07 +0100

    Edd experimental "EMMC install" script.

---
+++ files/cb2_install_emmc.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+TGTDEV=/dev/mmcblk0
+SRCDEV=/dev/mmcblk1
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
+g
+n
+
+32768
++2G
+n
+
+
+
+w
+EOF
+dd if=/mnt/cdrom/idbloader.img of= ${TGTDEV} bs=512 seek=64
+dd if=/mnt/cdrom/u-boot.itb of= ${TGTDEV} bs=512 seek=16384
+mkfs.ext4 ${TGTDEV}p1
+mkfs.ext4 ${TGTDEV}p2
+mount ${TGTDEV}p1 /media
+cp -R /mnt/cdrom/* /media/
+umount /media
+mount ${TGTDEV}p2 /media
+touch /media/.rw_part
+umount /media
